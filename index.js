3fe9143dfc9f2abe157adc464aea87db3ebf0cdd
meredithdean <meredithdvhamilton@gmail.com>
2023-02-06 17:42:38 +0000

    finished node-postgres setup

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require( 'express' )
+const bodyParser = require('body-parser')
 const app     = express()
 const port    = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true 
+})
+);
+
+app.use( ( req, res ) => {
+    res.type( 'text/plain' )
+    res.status( 404 )
+    res.send('404 Not found ☕_☕')
+    });
 
 app.get( '/' ,(req, res) => {
 res.type( 'text/plain' )
@@ -12,11 +25,5 @@ res.type( 'text/plain' )
 res.send( 'Server Expresso ☕ About')
 });
 
-app.use( ( req, res ) => {
-res.type( 'text/plain' )
-res.status( 404 )
-res.send('404 Not found ☕_☕')
-});
-
-app.listen( port ,
+app.listen(port,
 () => console.log(`Expresso ☕ is on Port ${ port } Ctrl + C to Stop `) );
