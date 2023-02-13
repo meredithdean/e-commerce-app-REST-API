f8370abe6efca3c45bce7e275bf3dc48f43afcd5
meredithdean <meredithdvhamilton@gmail.com>
2023-02-13 18:05:20 +0000

    fixed the server

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+require('dotenv/config');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const api = process.env.API_URL
+
+// middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url',
+    }
+    res.send(product)
+});
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body
+    console.log(newProduct)
+    res.send(newProduct)
+});
+
+app.listen(port, () => {
+    console.log(`Expresso ☕ is on Port ${ port } Ctrl + C to Stop `); 
+});
