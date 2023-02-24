5ffc1c701e999458f8c59b230c705cfcd7bc6b4a
meredithdean <meredithdvhamilton@gmail.com>
2023-02-24 12:45:28 +0000

    finished orders & management API routes

--- models/order.js
+++ models/order.js
@@ -1,13 +1,77 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItems',
         required: true
+    }],
+    shippingAddress1: {
+        type: String,
+        required: true
+    },
+    shippingAddress2: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending'
+    },
+    totalPrice: {
+        type: Number
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now 
     }
 });
 
 
 exports.Order = mongoose.model('Order', orderSchema);
+
+
+
+/**
+Order Example:
+{
+    "orderItems" : [
+        {
+            "quantity": 3,
+            "product" : "63ee5a713868e32919618eda"
+        },
+        {
+            "quantity": 2,
+            "product" : "63efa305c82d3bf86269a639"
+        }
+    ],
+    "shippingAddress1" : "Flowers Street , 45",
+    "shippingAddress2" : "1-B",
+    "city": "Prague",
+    "zip": "00000",
+    "country": "Czech Republic",
+    "phone": "+420702241333",
+    "user": "63f3927ef52fb90bfee4b0a2"
+}
+ */
